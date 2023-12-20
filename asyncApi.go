a2d3c346363b66e596ae0c90d126885a035e870c
Hardmun <spamnvg@gmail.com>
2023-12-20 20:20:23 +0300

    Lunamoon develop

--- asyncApi.go
+++ asyncApi.go
@@ -1,5 +1,23 @@
 package main
 
+import "os"
+
 func main() {
-	println("lol")
+	args := os.Args
+	switch len(args) {
+	case 2:
+		arg := args[1]
+
+		if arg == "-clearLogs" {
+			println("clearLogs")
+		} else {
+			println(arg)
+		}
+	case 3:
+
+	}
+	//argsLen := len(args)
+	//if argsLen == 2 {
+	//	println(2)
+	//}
 }
